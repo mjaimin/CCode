ac5f83bf7bbf1ebe8a4fea4a950c8c6239462dc9
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-06-15 02:50:50 +0900

    moved swap functionality in to function

--- Bitwise/XORSwap.c
+++ Bitwise/XORSwap.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+void swap(int *a,int *b)
 {
-	int x = 9 ,y = 10;
-	x=x^y;
-	y=y^x;
-	x=x^y;
-	printf("%d,%d\n",x,y);
+  *a = *a ^ *b;
+  *b = *a ^ *b;
+  *a = *a ^ *b;
+}
+int main()
+{
+  int a=5, b=6;
+  swap(&a,&b);
+  printf("a=%d, b=%d \n",a,b);
+  return 0;
 }
