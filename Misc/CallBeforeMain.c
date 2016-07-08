ca41d1f9d4aa269bee95ab1b368c704c6ecdd2bf
mjaimin <m.jaimin@gmail.com>
2016-07-08 11:29:43 +0530

    Create CallBeforeMain.c

---
+++ Misc/CallBeforeMain.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/* Apply the constructor attribute to myStartupFun() so that it
+    is executed before main() */
+void myStartupFun (void) __attribute__ ((constructor));
+
+
+/* Apply the destructor attribute to myCleanupFun() so that it
+   is executed after main() */
+void myCleanupFun (void) __attribute__ ((destructor));
+
+
+void myStartupFun (void)
+{
+    printf ("startup code before main()\n");
+}
+
+void myCleanupFun (void)
+{
+    printf ("cleanup code after main()\n");
+}
+
+int main (void)
+{
+    printf ("hello\n");
+    return 0;
+}
+//Output
+//startup code before main()
+//hello
+//cleanup code after main()
