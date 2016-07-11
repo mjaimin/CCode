75f0d2d96e0303b42fffe3c6e3613b0f779e95da
mjaimin <m.jaimin@gmail.com>
2016-07-11 17:37:26 +0530

    Create size_t.1.c

---
+++ Misc/size_t.1.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() 
+{
+   size_t sz = sizeof(long long);// size_t is unsigned int so, sz is never going to be lessthen Zero,
+                                 //rather --sz will roll over again to FFFF after sz is 0
+    while ( --sz >= 0 )
+    {
+        printf("Hello World\n");
+    }
+}
