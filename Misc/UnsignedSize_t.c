57bfe4f1f838f66862119622f221e9ead10f8053
mjaimin <m.jaimin@gmail.com>
2016-07-13 11:07:39 +0530

    Update UnsignedSize_t.c

--- Misc/UnsignedSize_t.c
+++ Misc/UnsignedSize_t.c
@@ -1,7 +1,4 @@
-#include <iostream>
 #include <stdio.h>
-using namespace std;
-
 int main() 
 {
    size_t sz = sizeof(long long);// size_t is unsigned int so it will never be less then 0 rather it will rollover from FFFF
