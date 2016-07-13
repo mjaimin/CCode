7bf0734736c0a13cfae118ee9c63417f34f841f4
mjaimin <m.jaimin@gmail.com>
2016-07-13 10:52:23 +0530

    Update BitFiledSizeof.c

--- BitField/BitFiledSizeof.c
+++ BitField/BitFiledSizeof.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 struct 
 {
-   unsigned char widthValidated : 1;
-   unsigned char heightValidated : 7;
+   unsigned char x1:1;
+   unsigned char x2:7;
 } status;
  
 struct 
 {
-   //unsigned char widthValidated : 0;// Error : named bit field cannot have zero width
-   //unsigned char heightValidated : 9; //Error: type of bit field too small for number of bits
+   //unsigned char x1:0;// Error : named bit field cannot have zero width
+   //unsigned char x2:9; //Error: type of bit field too small for number of bits
 } errorStruct;
 
 int main( ) 
 {
-   printf( "Memory size occupied by status : %d\n", sizeof(status));
-   printf( "Memory size occupied by errorStruct : %d\n", sizeof(errorStruct));
-   //"Memory size occupied by status : 1"
-   //"Memory size occupied by status : 1"
-   return 0;
+   printf( "sizeof(status) : %d\n", sizeof(status));
+   printf( "sizeof(errorStruct) : %d\n", sizeof(errorStruct));
 }
