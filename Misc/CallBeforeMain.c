9337d35adc5ad06cc2ce9d7fc81dca07adef9f18
mjaimin <m.jaimin@gmail.com>
2016-07-13 11:02:52 +0530

    Update CallBeforeMain.c

--- Misc/CallBeforeMain.c
+++ Misc/CallBeforeMain.c
@@ -4,26 +4,19 @@
     is executed before main() */
 void myStartupFun (void) __attribute__ ((constructor));
 
-
 /* Apply the destructor attribute to myCleanupFun() so that it
    is executed after main() */
 void myCleanupFun (void) __attribute__ ((destructor));
 
 
-void myStartupFun (void)
-{
+void myStartupFun (void){
     printf ("startup code before main()\n");
 }
-
-void myCleanupFun (void)
-{
+void myCleanupFun (void){
     printf ("cleanup code after main()\n");
 }
-
-int main (void)
-{
+int main (void){
     printf ("hello\n");
-    return 0;
 }
 //Output
 //startup code before main()
