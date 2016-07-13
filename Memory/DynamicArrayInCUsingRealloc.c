8e15f8e0a8bf706c0c3299c50e84eef6d7b1f622
mjaimin <m.jaimin@gmail.com>
2016-07-13 10:59:49 +0530

    Update DynamicArrayInCUsingRealloc.c

--- Memory/DynamicArrayInCUsingRealloc.c
+++ Memory/DynamicArrayInCUsingRealloc.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//  This would normally be used by the polygon logic
 int main(int argc, const char * argv[])
 {
-	//  This would normally be provided by the polygon
-	int lengthOfArray = 2;
+	int lengthOfArray = 3;
 	int *element = (int*)malloc(sizeof(int)*lengthOfArray);
-
 	element[0] = 0;
 	element[1] = 1;
 
-	element = (int*)realloc(element, sizeof(int)*(lengthOfArray+5));
+	element = (int*)realloc(element, sizeof(int)*(lengthOfArray+6));	
 	element[2] = 2;
 	element[3] = 3;
 	element[4] = 4;
-//	element[1] = 5;
-//	element[0] = 6;
+	element[5] = 5;
+	
 	printf("%d %d %d %d %d", element[0], element[1], element[2], element[3], element[4]);
 }
